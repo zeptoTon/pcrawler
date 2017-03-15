ca8d0798dda7337da59a3f4383c91719e55783ab
zeptoTon <tony.li@zeptomind.com>
2017-03-15 17:03:27 +0000

    format changed only, tab size from 2 to 4

--- main.py
+++ main.py
@@ -5,138 +5,141 @@ import urllib.request
 import re
 from functools import reduce
 
+
 class Crawler:
-  """
-  This crawler will follow configuration file for fetching data on HTML document
-  """
-  def __init__(self, filename):
-    """ load configuration """
-    self.config = self._load_configuration(filename)
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', self.config["user_agent"])]
-    self.opener = opener
-    self.result = {}
-    self.results = []
-
-  @staticmethod
-  def _load_configuration(filename):
-    """ load configuration to instance
-    Args:
-      filename: filename in current directory
-    Return:
-      config: dict foramt
-    Raise:
-      TODO: ConfigurationError if format incorrect
-      FileNotFoundError if file not find
     """
-    with open(filename) as json_data_file:
-      data = json.load(json_data_file)
-    return data
-
-  @staticmethod
-  def _extract_text_node(document, css_path, value_format, multiple=False):
-    """ return value of target node """
-    soup = BeautifulSoup(document, "html.parser")
-    els = soup.select(css_path)
-    if len(els):
-      if multiple:
-        return '\n'.join([x.text.strip() for x in els])
-      else:
-        if value_format == "number":
-          price = re.findall("[-+]?\d+[\.]?\d*", els[0].text.strip())
-          return float(price[0])
-        return els[0].text.strip()
-    else:
-      raise ValueError("Cannot find node css_path:%s" % css_path)
-
-  def _extract_link_node(self, document, css_path, nested_properties):
-    """ update dict value of nested target node """
-    a_links = BeautifulSoup(document, "html.parser").select(css_path)
-    if len(a_links) > 0:
-      url = a_links[0]["href"]
-      content = self.opener.open(url).read()
-      for p in nested_properties:
-        self._content_router(content, p)
-
-
-  def _content_router(self, document, l_property):
-    """ Set the result to instance
-    Args:
-      document: could be part of the html, does not need to be <html>
-      l_property: contain information of the lookup
+    This crawler will follow configuration file for fetching data on HTML document
     """
-    if l_property["type"] == "text":
-      self.result[l_property["name"]] = self._extract_text_node(document, l_property["css_path"], l_property["format"], l_property["multiple"])
-    elif l_property["type"] == "sizeof":
-      self.result[l_property["name"]] = self._extract_size_node(document)
-    elif l_property["type"] == "link":
-      self._extract_link_node(document, l_property["css_path"], l_property["nested_properties"])
 
-  @staticmethod
-  def _extract_size_node(document):
-    """ return size in kb of target document
-    Args:
-      document: content in string
-    Return:
-      config: size in kb
-    Raise:
-      ValueError if document is None
-    """
-    if not document is None:
-      return "%.2fkb" % (sys.getsizeof(document) * 8 / 1024)
-    else:
-      raise ValueError("Missing document input")
+    def __init__(self, filename):
+        """ load configuration """
+        self.config = self._load_configuration(filename)
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', self.config["user_agent"])]
+        self.opener = opener
+        self.result = {}
+        self.results = []
 
+    @staticmethod
+    def _load_configuration(filename):
+        """ load configuration to instance
+        Args:
+          filename: filename in current directory
+        Return:
+          config: dict foramt
+        Raise:
+          TODO: ConfigurationError if format incorrect
+          FileNotFoundError if file not find
+        """
+        with open(filename) as json_data_file:
+            data = json.load(json_data_file)
+        return data
 
-  def _output_results(self):
-    """ output result to txt file """
-    reducers = self.config["reducers"]
-    addon = {}
-    for r in reducers:
-      if r["type"] == "sum":
-        n = r['name']
-        k = r['key']
-        v = 0
-        for x in self.results:
-          v += x[k]
-        addon[n] = round(v,2)
+    @staticmethod
+    def _extract_text_node(document, css_path, value_format, multiple=False):
+        """ return value of target node """
+        soup = BeautifulSoup(document, "html.parser")
+        els = soup.select(css_path)
+        if len(els):
+            if multiple:
+                return '\n'.join([x.text.strip() for x in els])
+            else:
+                if value_format == "number":
+                    price = re.findall("[-+]?\d+[\.]?\d*", els[0].text.strip())
+                    return float(price[0])
+                return els[0].text.strip()
+        else:
+            raise ValueError("Cannot find node css_path:%s" % css_path)
 
-    to_dump = {
-      'results': self.results,
-    }
-    to_dump.update(addon)
+    def _extract_link_node(self, document, css_path, nested_properties):
+        """ update dict value of nested target node """
+        a_links = BeautifulSoup(document, "html.parser").select(css_path)
+        if len(a_links) > 0:
+            url = a_links[0]["href"]
+            content = self.opener.open(url).read()
+            for p in nested_properties:
+                self._content_router(content, p)
 
-    r_json = json.dumps(to_dump)
+    def _content_router(self, document, l_property):
+        """ Set the result to instance
+        Args:
+          document: could be part of the html, does not need to be <html>
+          l_property: contain information of the lookup
+        """
+        if l_property["type"] == "text":
+            self.result[l_property["name"]] = self._extract_text_node(
+                document, l_property["css_path"], l_property["format"], l_property["multiple"])
+        elif l_property["type"] == "sizeof":
+            self.result[l_property["name"]] = self._extract_size_node(document)
+        elif l_property["type"] == "link":
+            self._extract_link_node(
+                document, l_property["css_path"], l_property["nested_properties"])
 
-    filename = self.config["filename"]
-    with open(filename, 'w') as f:
-      f.write(r_json)
-    return to_dump
+    @staticmethod
+    def _extract_size_node(document):
+        """ return size in kb of target document
+        Args:
+          document: content in string
+        Return:
+          config: size in kb
+        Raise:
+          ValueError if document is None
+        """
+        if not document is None:
+            return "%.2fkb" % (sys.getsizeof(document) * 8 / 1024)
+        else:
+            raise ValueError("Missing document input")
 
-  def start(self):
-    """ This method will crawl data accroding to configuration file,
-    result will be output in directory
-    TODO: show % progress in stdout
-    1. crawl url -> root document,
-    2. pass to countent router to decide what to do
-    3. output result
-    """
-    url = self.config["document_url"]
-    l_properties = self.config["lookup_properties"]
-    content = self.opener.open(url).read()
-    items = BeautifulSoup(content, "html.parser").select(self.config["items_css_path"])
-    results = []
-    for item in items:
-      self.result = {}
-      for p in l_properties:
-        self._content_router(str(item), p)
-      results.append(self.result)
-    self.results = results
-    return self._output_results()
+    def _output_results(self):
+        """ output result to txt file """
+        reducers = self.config["reducers"]
+        addon = {}
+        for r in reducers:
+            if r["type"] == "sum":
+                n = r['name']
+                k = r['key']
+                v = 0
+                for x in self.results:
+                    v += x[k]
+                addon[n] = round(v, 2)
 
-if __name__ == "__main__" :
-  _config = sys.argv[1]
-  _crawler = Crawler(_config)
-  result_dict = _crawler.start()
-  # result_dict for external module,
-  # check sainsbury.json in directory
+        to_dump = {
+            'results': self.results,
+        }
+        to_dump.update(addon)
+
+        r_json = json.dumps(to_dump)
+
+        filename = self.config["filename"]
+        with open(filename, 'w') as f:
+            f.write(r_json)
+        return to_dump
+
+    def start(self):
+        """ This method will crawl data accroding to configuration file,
+        result will be output in directory
+        TODO: show % progress in stdout
+        1. crawl url -> root document,
+        2. pass to countent router to decide what to do
+        3. output result
+        """
+        url = self.config["document_url"]
+        l_properties = self.config["lookup_properties"]
+        content = self.opener.open(url).read()
+        items = BeautifulSoup(content, "html.parser").select(
+            self.config["items_css_path"])
+        results = []
+        for item in items:
+            self.result = {}
+            for p in l_properties:
+                self._content_router(str(item), p)
+            results.append(self.result)
+        self.results = results
+        return self._output_results()
+
+if __name__ == "__main__":
+    _config = sys.argv[1]
+    _crawler = Crawler(_config)
+    result_dict = _crawler.start()
+    # result_dict for external module,
+    # check sainsbury.json in directory
--- test.py
+++ test.py
@@ -7,105 +7,104 @@ import json
 
 
 class TestStringMethods(unittest.TestCase):
-  """
-  Test Case has coverd all function, we use mock to replace url reqeust.
-  """
-  def setUp(self):
-    with open('test/htmldoc.txt', 'r') as d:
-      self.doc = d.read()
-    self.crawler = Crawler("test/test_correct.config.json")
-    self.result = {}
+    """
+    Test Case has coverd all function, we use mock to replace url reqeust.
+    """
 
-  def tearDown(self):
-    pass
+    def setUp(self):
+        with open('test/htmldoc.txt', 'r') as d:
+            self.doc = d.read()
+        self.crawler = Crawler("test/test_correct.config.json")
+        self.result = {}
 
-  def test_loading_config(self):
-    self.assertRaises(FileNotFoundError, Crawler._load_configuration,
-    "test/test_wrong.config.txt")
+    def tearDown(self):
+        pass
 
+    def test_loading_config(self):
+        self.assertRaises(FileNotFoundError, Crawler._load_configuration,
+                          "test/test_wrong.config.txt")
 
-  def test_extract_size_node(self):
-    self.assertEqual("660.87kb", Crawler._extract_size_node(self.doc))
+    def test_extract_size_node(self):
+        self.assertEqual("660.87kb", Crawler._extract_size_node(self.doc))
 
-  def test_extract_text_node(self):
-    self.assertEqual("Sainsbury's Apricot Ripe & Ready x5",
-    Crawler._extract_text_node(self.doc,
-    "#productLister > ul > li:nth-of-type(1) > div.product"+
-    " > div > div.productInfoWrapper > div > h3 > a",
-    "string"))
-    self.assertEqual(1.50, Crawler._extract_text_node(self.doc,
-     "#addItem_572163 > div.pricing > p.pricePerUnit", "number"))
+    def test_extract_text_node(self):
+        self.assertEqual("Sainsbury's Apricot Ripe & Ready x5",
+                         Crawler._extract_text_node(self.doc,
+                                                    "#productLister > ul > li:nth-of-type(1) > div.product" +
+                                                    " > div > div.productInfoWrapper > div > h3 > a",
+                                                    "string"))
+        self.assertEqual(1.50, Crawler._extract_text_node(self.doc,
+                                                          "#addItem_572163 > div.pricing > p.pricePerUnit", "number"))
 
-  @patch("urllib.request.OpenerDirector.open")
-  def test_extract_link_node(self, mock_open):
-    a = Mock()
-    with open("test/example1.html") as c:
-      e1 = c.read()
-    with open("test/example2.html") as c:
-      e2 = c.read()
-    a.read.side_effect = [e1, e2]
-    mock_open.return_value = a
-    nested_properties = [
-        {
-          "type" : "sizeof",
-          "name" : "size",
-          "format" : "string"
-        },
-        {
-          "type" : "text",
-          "name" : "description",
-          "multiple" : True,
-          "format" : "string",
-          "css_path" : "div.description > p"
+    @patch("urllib.request.OpenerDirector.open")
+    def test_extract_link_node(self, mock_open):
+        a = Mock()
+        with open("test/example1.html") as c:
+            e1 = c.read()
+        with open("test/example2.html") as c:
+            e2 = c.read()
+        a.read.side_effect = [e1, e2]
+        mock_open.return_value = a
+        nested_properties = [
+            {
+                "type": "sizeof",
+                "name": "size",
+                "format": "string"
+            },
+            {
+                "type": "text",
+                "name": "description",
+                "multiple": True,
+                "format": "string",
+                "css_path": "div.description > p"
+            }
+        ]
+        css_path = "#productLister > ul > li:nth-of-type(3) > div.product > div > div.productInfoWrapper > div > h3 > a"
+
+        expected_result = {
+            "size": "2.05kb",
+            "description": "E\nX\nA\nM\nP\nL\nE\n1",
         }
-    ]
-    css_path = "#productLister > ul > li:nth-of-type(3) > div.product > div > div.productInfoWrapper > div > h3 > a"
+        self.crawler._extract_link_node(self.doc, css_path, nested_properties)
+        for k in expected_result:
+            self.assertEqual(self.crawler.result[k], expected_result[k])
 
-    expected_result = {
-      "size" : "2.05kb",
-      "description" : "E\nX\nA\nM\nP\nL\nE\n1",
-    }
-    self.crawler._extract_link_node(self.doc, css_path, nested_properties)
-    for k in expected_result:
-      self.assertEqual(self.crawler.result[k], expected_result[k])
-
-
-  @patch("urllib.request.OpenerDirector.open")
-  def test_start(self, mock_open):
-    a = Mock()
-    with open("test/example.html") as c:
-      ex = c.read()
-    with open("test/example1.html") as c:
-      e1 = c.read()
-    with open("test/example2.html") as c:
-      e2 = c.read()
-    a.read.side_effect = [ex, e1, e2]
-    mock_open.return_value = a
-    r = {
-      "results": [
-        {
-          "title":  "Example 1",
-          "size": "2.05kb",
-          "unit_price": 11.11,
-          "description": "E\nX\nA\nM\nP\nL\nE\n1"
-        },
-        {
-          "title":  "Example 2",
-          "size": "2.05kb",
-          "unit_price": 22.22,
-          "description": "E\nX\nA\nM\nP\nL\nE\n2"
+    @patch("urllib.request.OpenerDirector.open")
+    def test_start(self, mock_open):
+        a = Mock()
+        with open("test/example.html") as c:
+            ex = c.read()
+        with open("test/example1.html") as c:
+            e1 = c.read()
+        with open("test/example2.html") as c:
+            e2 = c.read()
+        a.read.side_effect = [ex, e1, e2]
+        mock_open.return_value = a
+        r = {
+            "results": [
+                {
+                    "title":  "Example 1",
+                    "size": "2.05kb",
+                    "unit_price": 11.11,
+                    "description": "E\nX\nA\nM\nP\nL\nE\n1"
+                },
+                {
+                    "title":  "Example 2",
+                    "size": "2.05kb",
+                    "unit_price": 22.22,
+                    "description": "E\nX\nA\nM\nP\nL\nE\n2"
+                }
+            ],
+            "total": 33.33
         }
-      ],
-      "total": 33.33
-    }
-    self.assertEqual(self.crawler.start(), r)
+        self.assertEqual(self.crawler.start(), r)
 
-  def test_output_results(self):
-    with open('test/test_sainsbury.json') as c:
-      t = json.load(c)
-    with open('test/example_result.json') as c:
-      r = json.load(c)
-    self.assertEqual(t, r)
+    def test_output_results(self):
+        with open('test/test_sainsbury.json') as c:
+            t = json.load(c)
+        with open('test/example_result.json') as c:
+            r = json.load(c)
+        self.assertEqual(t, r)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
